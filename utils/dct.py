14036c366378bd5af0f598c651a8516e75007102
TwinklingWind <141333831+TwinklingWind@users.noreply.github.com>
2026-04-20 16:00:07 +0800

    Update dct.py

--- utils/dct.py
+++ utils/dct.py
@@ -4,17 +4,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def mydct(img):
-
-    h, w = img.shape[:2]
     # 读取图像
     # b, g, r = cv2.split(img)
     # img = cv2.merge((r, g, b))
 
+    keep_fraction = 0.2
+
+    h, w = img.shape[:2]
+    keep_h = max(1, int(h * keep_fraction))
+    keep_w = max(1, int(w * keep_fraction))
+
     img1 = img[:, :, 0]
     img2 = img[:, :, 1]
     img3 = img[:, :, 2]
+
+
+
 
     # 数据类型转换 转换为浮点型
     # print('0\n', img)
@@ -22,27 +28,42 @@ def mydct(img):
     img2 = img2.astype(np.float32)
     img3 = img3.astype(np.float32)
 
+
+
     # 进行离散余弦变换
     img_dct1 = cv2.dct(img1)
     img_dct2 = cv2.dct(img2)
     img_dct3 = cv2.dct(img3)
     # print('1\n', img_dct)
 
-    keep_h = max(1, int(h * 0.8))
-    keep_w = max(1, int(w * 0.8))
 
-    img_dct1[keep_h:, keep_w:] = 0
-    img_dct2[keep_h:, keep_w:] = 0
-    img_dct3[keep_h:, keep_w:] = 0
+    dct_mask1 = np.zeros_like(img_dct1)
+    dct_mask1[:keep_h, :keep_w] = img_dct1[:keep_h, :keep_w]
+    dct_mask1[:keep_h, keep_w:] = img_dct1[:keep_h, keep_w:] * 0.8
+    dct_mask1[keep_h:, :] = img_dct1[keep_h:, :] * 0.8
 
-    img_idct1 = cv2.idct(img_dct1)
-    img_idct2 = cv2.idct(img_dct2)
-    img_idct3 = cv2.idct(img_dct3)
+
+    dct_mask2 = np.zeros_like(img_dct2)
+    dct_mask2[:keep_h, :keep_w] = img_dct2[:keep_h, :keep_w]
+    dct_mask2[:keep_h, keep_w:] = img_dct2[:keep_h, keep_w:] * 0.8
+    dct_mask2[keep_h:, :] = img_dct2[keep_h:, :] * 0.8
+
+
+    dct_mask3 = np.zeros_like(img_dct3)
+    dct_mask3[:keep_h, :keep_w] = img_dct3[:keep_h, :keep_w]
+    dct_mask3[:keep_h, keep_w:] = img_dct3[:keep_h, keep_w:] * 0.8
+    dct_mask3[keep_h:, :] = img_dct3[keep_h:, :] * 0.8
+
+    # 进行离散余弦反变换
+    img_idct1 = cv2.idct(dct_mask1)
+    img_idct2 = cv2.idct(dct_mask2)
+    img_idct3 = cv2.idct(dct_mask3)
+    # print('3\n', img_idct)
+    # res = img_idct.astype(np.uint8) # 浮点型转整型 小数部分截断
+    # print('3-1\n',res)
 
     nowimg = np.stack([img_idct1, img_idct2, img_idct3])
     nowimg = np.transpose(nowimg, (1, 2, 0))
     nowimg = nowimg.astype(np.uint8)
 
     return nowimg
-
-
